189920153c3e1889dc0e85de74149f921716c51f
Falderian <nikitastaselovich@gmail.com>
2022-11-04 17:19:58 +0300

    feat: add sort to cities

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import store from 'helpers/redux/store';
 import { ICities } from 'interfaces/cities';
 import { IState } from 'interfaces/store';
-import React, { FormEvent, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AgreementSection } from './agreementSection/agreementSection';
 import { CitySelect } from './citySelect/citySelect';
@@ -11,21 +12,42 @@ import { SubmitSection } from './submitSection/submitSection';
 
 export const Form = () => {
   const [cities, setCities] = useState<ICities>([]);
-  const state = useSelector<IState>((state) => state);
+
+  useEffect(() => {
+    const cities = getCities();
+    cities.then((data) => {
+      const sortedCities = sortCities(data);
+      setCities(sortedCities);
+    });
+  }, []);
 
   const getCities = async () => {
     const response = await axios.get('https://json.extendsclass.com/bin/83c9ce2d0a7b');
     return response.data;
   };
 
-  useEffect(() => {
-    const cities = getCities();
-    cities.then((data) => setCities(data));
-  });
-
   const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    console.log(state);
+    const result = Object.assign(store.getState().appData);
+    console.log(result);
+  };
+
+  const sortCities = (cities: ICities) => {
+    cities = cities.filter((el) => Number(el.population) > 50000);
+    const swapedCities = swapTwoCities(cities);
+    const sotredCities = swapedCities.sort();
+    return sotredCities;
+  };
+
+  const swapTwoCities = (cities: ICities) => {
+    let max = cities[0];
+    for (let i = 0; i < cities.length; i++) {
+      if (cities[i] >= max) max = cities[i];
+    }
+    const temp = cities[0];
+    cities[cities.indexOf(max)] = temp;
+    cities[0] = max;
+    return cities;
   };
 
   return (
--- src/components/status/status.tsx
+++ src/components/status/status.tsx
@@ -1,9 +1,11 @@
 import { statusReducer } from 'helpers/redux/appSlice';
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 export const Status = () => {
   const dispatch = useDispatch();
+
+  const [activeTextfield, setActiveTextfield] = useState(true);
 
   const handleOnBlur = (evt: React.FocusEvent<HTMLTextAreaElement>) => {
     dispatch(
@@ -17,9 +19,15 @@ export const Status = () => {
     <section className="status">
       <div className="status__head">
         <h1 className="status__greetings">Здравствуйте, Человек №3596941</h1>
-        <button className="status__change-btn">Сменить статус</button>
+        <button className="status__change-btn" onClick={() => setActiveTextfield(!activeTextfield)}>
+          Сменить статус
+        </button>
       </div>
-      <textarea className="status__textfield" onBlur={(evt) => handleOnBlur(evt)}></textarea>
+      <textarea
+        disabled={activeTextfield}
+        className="status__textfield"
+        onBlur={(evt) => handleOnBlur(evt)}
+      ></textarea>
     </section>
   );
 };
